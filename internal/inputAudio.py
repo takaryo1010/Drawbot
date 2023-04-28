4009cb026726c42e42f76a9cdf664f699fc80170
takaryo1010 <t.ryota1010@gmail.com>
2023-04-28 19:42:47 +0900

    add:inputAudio<-keypressed

--- internal/inputAudio.py
+++ internal/inputAudio.py
@@ -1,27 +1,30 @@
 import time
 import speech_recognition
 import pyaudio
+import keyboard as key
 from dataclasses import dataclass
+
 
 @dataclass
 class RequestAudio:
-    def __init__(self):
+    def __init__(self):  # デフォルトの値
         self.SAMPLERATE = 44100
         self.sprec = speech_recognition.Recognizer()
-        self.IsInput = True
+        self.isInput = False
         self.List_inputs = []
 
+    def callback(self, in_data, frame_count, time_info, status):
 
-    
-    def callback(self,in_data, frame_count, time_info, status):
-
-        if self.IsInput!=True:
+        if self.isInput != True:
             return (None, pyaudio.paContinue)
-            
         try:
-            audiodata = speech_recognition.AudioData(in_data,self.SAMPLERATE,2)
-            sprec_text = self.sprec.recognize_google(audiodata, language='ja-JP')
+            audiodata = speech_recognition.AudioData(
+                in_data, self.SAMPLERATE, 2)
+            sprec_text = self.sprec.recognize_google(
+                audiodata, language='ja-JP')
             self.List_inputs.append(sprec_text)
+
+            # TODO GUIにするときは、リストを上から表示させたい
             print(self.List_inputs)
 
         except speech_recognition.UnknownValueError:
@@ -30,26 +33,31 @@ class RequestAudio:
             pass
         finally:
             return (None, pyaudio.paContinue)
-        
-        
-        
-def IsPressEnter():
-    return True
+
 
 def InputAudio(reqAudio):
     audio = pyaudio.PyAudio()     # Audio インスタンス取得
-    stream = audio.open( format = pyaudio.paInt16,
-                        rate = reqAudio.SAMPLERATE,
-                        channels = 1, 
-                        input_device_index = 1,
-                        input = True, 
-                        frames_per_buffer =reqAudio.SAMPLERATE*2, # 2秒周期でコールバック
+    stream = audio.open(format=pyaudio.paInt16,
+                        rate=reqAudio.SAMPLERATE,
+                        channels=1,
+                        input_device_index=1,
+                        input=True,
+                        frames_per_buffer=reqAudio.SAMPLERATE*2,  # 2秒周期でコールバック
                         stream_callback=reqAudio.callback)
     stream.start_stream()
+
     while stream.is_active():
-        if IsPressEnter()!=True:#エンターキーが押されたらbreak
+        if key.is_pressed("enter"):  # エンターキーが押されたらbreak
             break
+        
+        if key.is_pressed("space"):
+            reqAudio.isInput = True
+        else:
+            reqAudio.isInput = False
         time.sleep(0.1)
+
     stream.stop_stream()
     stream.close()
     audio.terminate()
+
+    return reqAudio.List_inputs
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import time
 import speech_recognition
 import pyaudio
-from internal import inputAudio as iA
+from internal import inputAudio
 
 
+def main():
+    reqAudio = inputAudio.RequestAudio()  # インスタンスを生成
 
+    inputAudio.InputAudio(reqAudio)
 
-
-def main(): 
-    reqAudio=iA.RequestAudio() # インスタンスを生成
-
-    iA.InputAudio(reqAudio)
-    
     # req.List_inputsの要素をDeeplで翻訳
     # Deeplで翻訳されたリストをChatGPTに投げる
 
-    
+
 if __name__ == '__main__':
     main()
